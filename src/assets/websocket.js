d52c5ed556019b629b5c1b378b58f59fce69f11c
Chenger1 <vladislav.marahovskiy@gmail.com>
2025-05-15 19:49:24 +0300

    Feature (Websocket):  - add event to send data from textfield via websocket to server

--- src/assets/websocket.js
+++ src/assets/websocket.js
@@ -1,9 +1,23 @@
 function WebsocketHandler(){
     this.connection = null;
+    this.signalForm = document.getElementById('signalForm');
 
     this.connect = function(){
         this.connection = new WebSocket("ws://localhost:8000/ws");
         this.connection.onmessage = function(e){ console.log(e.data); };
-        this.connection.onopen = () => this.connection.send('hello');
+        this.connection.onopen = () => this.onOpen();
+    }
+
+    this.onOpen = function(){
+        this.connection.send('hello');
+
+        const This = this;
+        document.getElementById('sendSignal').addEventListener('click', function(){
+            const signal = {
+                'topic': 'userSignal',
+                'signal': This.signalForm.signal.value
+            }
+            This.connection.send(JSON.stringify(signal));
+        })
     }
 }
